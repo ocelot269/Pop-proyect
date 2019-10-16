6c74653afacabcdbbb5c26b0de0e27f03a851d5c
unknown <ocelot269@gmail.com>
2019-10-16 17:02:46 +0200

    agregado  json al local storage

--- Back/Js/proyecto.js
+++ Back/Js/proyecto.js
@@ -1,27 +1,23 @@
 "use strict";
 
 var pulpo;
-var registro = [
-  {
-    evento: "trabajar, tocar un árbol, hamburguesa, correr, twitter",
-    pulpo: false
-  }
-];
+var dataFromlocalStorage = JSON.parse(localStorage.getItem("diario"));
 
-function contieneEvento($evento, evento) {}
 
-function mostrarRegistro() {}
+function contieneEvento($evento) { }
+
+function mostrarRegistro() { }
 
 // Añade un objeto de tipo Registro al diario.
-function mostrarDiario() {}
+function mostrarDiario() { }
 
 function phi(n00, n01, n10, n11) {
-  let multiplos = (n10 + n11) * 
-                  (n01 + n00) * 
-                  (n01 + n11) * 
-                  (n10 + n00);
-                  
-  return (n11 * n00 - n10 * n01) / Math.sqrt(multiplos);
-}
+  let discriminante = (n10 + n11) *
+    (n01 + n00) *
+    (n01 + n11) *
+    (n10 + n00);
 
+  return (n11 * n00 - n10 * n01) / Math.sqrt(discriminante);
+}
+console.log(DIARIO);
 console.log(phi(76, 9, 4, 1));

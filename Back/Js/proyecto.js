16994819ec05c411d8a9ea13b9ec6728479f0327
unknown <ocelot269@gmail.com>
2019-10-16 18:34:41 +0200

    agregada funcionalidad

--- Back/Js/proyecto.js
+++ Back/Js/proyecto.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict'
 
 var pulpo;
 var dataFromlocalStorage = JSON.parse(localStorage.getItem("diario"));
-
+var conjuntoN = [];
 
 function contieneEvento($evento) { }
 
@@ -10,6 +10,16 @@ function mostrarRegistro() { }
 
 // Añade un objeto de tipo Registro al diario.
 function mostrarDiario() { }
+
+function calcularN00() {
+  let n00 = new Set([]);
+  DIARIO.forEach(evento => {
+    n00 = new Set([]);;
+    n00.add(evento.eventos);
+    console.log(n00);
+    
+  });
+}
 
 function phi(n00, n01, n10, n11) {
   let discriminante = (n10 + n11) *
@@ -19,5 +29,8 @@ function phi(n00, n01, n10, n11) {
 
   return (n11 * n00 - n10 * n01) / Math.sqrt(discriminante);
 }
-console.log(DIARIO);
+
+
 console.log(phi(76, 9, 4, 1));
+console.log(calcularN00());
+

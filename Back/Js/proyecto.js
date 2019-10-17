813b0ea02a4ab443166e83605b9f0d60e3113adf
unknown <ocelot269@gmail.com>
2019-10-17 23:22:27 +0200

    creado varios metodos pendiente de refactorizar nombres de metodos y variables

--- Back/Js/proyecto.js
+++ Back/Js/proyecto.js
@@ -1,34 +1,65 @@
-'use strict'
+"use strict";
 
-var pulpo;
 var dataFromlocalStorage = JSON.parse(localStorage.getItem("diario"));
+// set de acciones
 var listadoAccionesMariano = () => {
-  let acciones  = new Set([]);
+  let acciones = new Set([]);
   DIARIO.forEach(evento => {
     evento.eventos.forEach(accion => {
       acciones.add(accion);
     });
   });
   return acciones;
+};
+
+function obtenerListadoAccionesMarianoConValues() {
+  let listadoDefinitivo = [];
+  listadoAccionesMariano().forEach(registro => {
+     listadoDefinitivo.push(calcularN(registro));
+  });
+  return listadoDefinitivo;
 }
 
-function contieneEvento($evento) { }
+function calcularN(registro) {
+  let contieneNoPulpo = 0;
+  let contieneYPulpo = 0;
+  let noContieneYpulpo = 0;
+  let noContieneNoPulpo = 0;
+  DIARIO.forEach(evento => {
+    if (evento.eventos.includes(registro)) {
+      if (evento.pulpo) {
+        contieneYPulpo++;
+      } else {
+        contieneNoPulpo++;
+      }
+    } else if (!evento.eventos.includes(registro)) {
+      if (evento.pulpo) {
+        noContieneYpulpo++;
+      } else {
+        noContieneNoPulpo++;
+      }
+    }
+  });
+  return { nombre: registro, value: { n00: noContieneNoPulpo, n01: contieneNoPulpo, n10: noContieneYpulpo, n11: contieneYPulpo }
+  };
+}
 
-function mostrarRegistro() { }
+function contieneEvento($evento) {}
 
-// Añade un objeto de tipo Registro al diario.
-function mostrarDiario() { }
+function mostrarRegistro() {}
+
+function mostrarValoresPhi() {
+  let listado = obtenerListadoAccionesMarianoConValues();
+  listado.forEach(elemento => {
+    console.log(elemento ,phi(elemento.value.n00,elemento.value.n01,elemento.value.n10,elemento.value.n11));
+  });
+}
 
 function phi(n00, n01, n10, n11) {
-  let discriminante = (n10 + n11) *
-    (n01 + n00) *
-    (n01 + n11) *
-    (n10 + n00);
+  let discriminante = (n10 + n11) * (n01 + n00) * (n01 + n11) * (n10 + n00);
 
   return (n11 * n00 - n10 * n01) / Math.sqrt(discriminante);
 }
 
-
 console.log(phi(76, 9, 4, 1));
-console.log(listadoAccionesMariano());
-
+console.log(mostrarValoresPhi());
